49b2c464d209e4d099a6c9ef942a87edc3046159
Vadim <aonyko@gmail.com>
2026-04-29 14:44:27 +0300

    com init 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,10 +11,36 @@ public class Main {
         tasks.add("Save progress");
 
         Queue<String> queue = new ArrayDeque<>(tasks);
-
-        while (!queue.isEmpty()) {
+        int limit = 10;
+        while (!queue.isEmpty() && count <= limit) {
             String task = queue.poll();
-            System.out.println((count++)+ ")In progress: " + task);
+            System.out.println((count++) + ") In progress: " + task);
+
+            if (task.equals("Looting drop")) {
+                System.out.println("Nothing to loot?Kill next monster");
+                queue.add("Fight with enemy");
+            } else if (task.equals("Repair items")) {
+                boolean repaired = false;
+                if (!repaired) {
+                    System.out.println("Nothing to repair");
+                    queue.add("Going to some store or market");
+                }
+            } else if (task.equals("Return to base")) {
+                System.out.println("Heading home...");
+            }
+            else if (task.equals("Save progress")) {
+                System.out.println("Game saved!");
+            }
+            else if (task.equals("Fight with enemy")) {
+                System.out.println("Fighting...");
+            }
+            else if (task.equals("Going to some store or market")) {
+                System.out.println("Buying materials...");
+                queue.add("Repair items");
+            }
+            else {
+                System.out.println("Unknown task");
+            }
         }
     }
 }
